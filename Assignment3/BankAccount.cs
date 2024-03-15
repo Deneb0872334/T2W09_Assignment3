d9b502cadc3e8f59874334b3cdcf9efcaf64c994
Deneb0872334 <c0872334@mylambton.ca>
2024-03-15 00:48:49 -0400

    Added: BankAccount
    - added the Bank Account class
    - overloaded the constructors
    - added the withdraw and deposit methods, no method body yet

---
+++ Assignment3/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Assignment3
+{
+	public class BankAccount
+	{
+        /*
+		  - Account Number
+		  - Balance
+		  - Type
+		 */
+        private string accountNumber;
+        private double balance;
+        private Type type = Type.Checking;
+
+        // the constructor is supposed to be public to be accessed from outside of the class
+        private BankAccount(string paramAccountNumber, double paramBalance, Type paramType)
+        {
+            this.accountNumber = paramAccountNumber;
+            this.balance = paramBalance;
+            this.type = paramType;
+        }
+
+        // delegating the construction of the instance to the other constructor
+        private BankAccount(string paramAccountNumber, double paramBalance) : this(paramAccountNumber, paramBalance, Type.Checking)
+        { }
+
+        public BankAccount()
+        { }
+
+        public void Deposit(double paramAmount)
+        { }
+
+        public bool Withdraw(double paramAmount)
+        {
+            return true;
+        }
+
+        public enum Type { Checking, Savings }
+    }
+}
+
---
+++ Assignment3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml.Linq;
+
+namespace Assignment3
+{
+    class OOP
+    {
+        public static void Main(string[] args)
+        {
+            // new keyword is for creating a new instance of the class
+            Car testCar = new Car();
+
+            Car bmw = new Car("BMW");
+            bmw.CarColor = Car.Color.BLUE;
+            // Speed property is private set
+            //bmw.Speed = 30;
+            PrintCarDetails(bmw);
+            bmw.Accelerate(30);
+            bmw.ReduceSpeed(40);
+            PrintCarDetails(bmw);
+
+            Car porsche = new Car(350, 0, "Porsche", Car.Type.Manual);
+            PrintCarDetails(porsche);
+
+            //Car errorProneCar = new Car(speed: 100, name: "test", type: "Hello");
+            //PrintCarDetails(errorProneCar);
+
+        }
+
+
+        private static void PrintCarDetails(Car car)
+        {
+            Console.WriteLine(
+                $"The car below is created:\n" +
+                $"Max Speed: {car.MaxSpeed}\n" +
+                $"Speed: {car.Speed}\n" +
+                $"Name: {car.Name}\n" +
+                $"Type: {car.CarType}\n" +
+                $"Gas Amout: {car.GasAmount}\n" +
+                $"Tank Size: {car.TankSize}\n\n"
+            );
+        }
+    }
+}
